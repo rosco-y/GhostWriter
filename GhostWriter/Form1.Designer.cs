f387e737b585573e0d301920bdfa42dd7e798c64
Ross Ylitalo <ross.ylitalo@gmail.com>
2023-04-25 06:38:02 -0400

    Simple Textbox, always deletes 1st of > 3 lines of text.

---
+++ GhostWriter/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace GhostWriter
+{
+    partial class frmGhostWriter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGhostWriter));
+            this.tmrElapsedTime = new System.Windows.Forms.Timer(this.components);
+            this.TemporaryText = new System.Windows.Forms.TextBox();
+            this.TopLineMenu = new System.Windows.Forms.MenuStrip();
+            this.mnuClear = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuClearAllText = new System.Windows.Forms.ToolStripMenuItem();
+            this.TopLineMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmrElapsedTime
+            // 
+            this.tmrElapsedTime.Interval = 1000;
+            // 
+            // TemporaryText
+            // 
+            this.TemporaryText.AcceptsReturn = true;
+            this.TemporaryText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TemporaryText.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TemporaryText.Location = new System.Drawing.Point(0, 33);
+            this.TemporaryText.Multiline = true;
+            this.TemporaryText.Name = "TemporaryText";
+            this.TemporaryText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TemporaryText.Size = new System.Drawing.Size(800, 417);
+            this.TemporaryText.TabIndex = 0;
+            this.TemporaryText.TextChanged += new System.EventHandler(this.TemporaryText_TextChanged);
+            // 
+            // TopLineMenu
+            // 
+            this.TopLineMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuClear});
+            this.TopLineMenu.Location = new System.Drawing.Point(0, 0);
+            this.TopLineMenu.Name = "TopLineMenu";
+            this.TopLineMenu.ShowItemToolTips = true;
+            this.TopLineMenu.Size = new System.Drawing.Size(800, 33);
+            this.TopLineMenu.TabIndex = 1;
+            this.TopLineMenu.Text = "menuStrip1";
+            // 
+            // mnuClear
+            // 
+            this.mnuClear.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuClearAllText});
+            this.mnuClear.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mnuClear.Name = "mnuClear";
+            this.mnuClear.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.mnuClear.Size = new System.Drawing.Size(68, 29);
+            this.mnuClear.Text = "Clear";
+            this.mnuClear.Click += new System.EventHandler(this.mnuClear_Click);
+            // 
+            // mnuClearAllText
+            // 
+            this.mnuClearAllText.Name = "mnuClearAllText";
+            this.mnuClearAllText.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.mnuClearAllText.Size = new System.Drawing.Size(210, 30);
+            this.mnuClearAllText.Text = "All Text";
+            this.mnuClearAllText.Click += new System.EventHandler(this.mnuClearAllText_Click);
+            // 
+            // frmGhostWriter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TemporaryText);
+            this.Controls.Add(this.TopLineMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.TopLineMenu;
+            this.Name = "frmGhostWriter";
+            this.Text = "GhostWriter";
+            this.TopLineMenu.ResumeLayout(false);
+            this.TopLineMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer tmrElapsedTime;
+        private System.Windows.Forms.TextBox TemporaryText;
+        private System.Windows.Forms.MenuStrip TopLineMenu;
+        private System.Windows.Forms.ToolStripMenuItem mnuClear;
+        private System.Windows.Forms.ToolStripMenuItem mnuClearAllText;
+    }
+}
+
---
+++ GhostWriter/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GhostWriter
+{
+    public partial class frmGhostWriter : Form
+    {
+        Stopwatch _timer = new Stopwatch();
+        TimeSpan _allowedTime = new TimeSpan(0, 0, 30);
+        public frmGhostWriter()
+        {
+            InitializeComponent();
+            _timer.Start();
+            tmrElapsedTime.Start();
+        }
+
+        private void mnuClear_Click(object sender, EventArgs e)
+        {
+            deleteFirstLine();
+        }
+
+        private void deleteFirstLine()
+        {
+            string[] lines = TemporaryText.Lines;
+            TemporaryText.Lines = lines.Skip<string>(1).ToArray();
+            TemporaryText.SelectionStart = TemporaryText.TextLength;
+            TemporaryText.ScrollToCaret();
+        }
+
+        private void mnuClearAllText_Click(object sender, EventArgs e)
+        {
+            TemporaryText.Lines = new string[0];
+        }
+
+        private void TemporaryText_TextChanged(object sender, EventArgs e)
+        {
+            if (TemporaryText.Lines.Length > 3)
+                deleteFirstLine();
+        }
+    }
+}
